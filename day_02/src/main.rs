0e4afc8d9b7583051f4b1ce640195ca4462d9eb7
fcalvez <calvez.freddy@gmail.com>
2019-12-02 16:38:01 +0100

    Don't Repeat Yourself + better loop for all values of `noun` and `verb` variables

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, Read, Write};
+use std::error::Error;
 
-type Result<T> = ::std::result::Result<T, Box<dyn (::std::error::Error)>>;
+type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
 fn main() -> Result<()> {
     let mut input = String::new();
@@ -25,8 +26,44 @@ fn part_1(input: &str) -> Result<()> {
     steps[1] = 12;
     steps[2] = 2;
 
+    let result = execute_intcode(steps)?;
 
-    // Next, execute the program until the value "99" is found
+    writeln!(io::stdout(), "Part 1 : {}", result)?;
+
+    Ok(())
+}
+
+fn part_2(input: &str) -> Result<()> {
+    let mut steps: Vec<usize> = vec![];
+
+    for line in input.lines() {
+        for step in line.split(',') {
+            steps.push(step.parse()?);
+        }
+    }
+
+    let expected_result = 19690720;
+
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            steps[1] = noun;
+            steps[2] = verb;
+
+            if let Ok(result) = execute_intcode(steps.clone()) {
+                if result == expected_result {
+                    writeln!(io::stdout(), "Part 2 : {}", 100 * noun + verb)?;
+                    return Ok(());
+                }
+            }
+        }
+    }
+
+    Err(Box::<Error>::from(format!("IntCode could not find expected value!")))
+}
+
+
+fn execute_intcode(mut steps: Vec<usize>) -> Result<usize> {
+    // Execute the program until the value "99" is found or an error occured
     let mut current_step: usize  = 0;
 
     loop {
@@ -72,8 +109,7 @@ fn part_1(input: &str) -> Result<()> {
                 steps[destination_step_position] = steps[first_step_position] * steps[second_step_position];
             }
             99 => {
-                writeln!(io::stdout(), "Part 1 : {}", steps[0])?;
-                break;
+                return Ok(steps[0]);
             }
             _ => {
                 eprintln!("Unknown opcode : {:?}", steps[current_step]);
@@ -84,107 +120,5 @@ fn part_1(input: &str) -> Result<()> {
         current_step += 4;
     }
 
-    Ok(())
-}
-
-fn part_2(input: &str) -> Result<()> {
-    let mut steps: Vec<usize> = vec![];
-
-    for line in input.lines() {
-        for step in line.split(',') {
-            steps.push(step.parse()?);
-        }
-    }
-
-    let original_steps = steps.clone(); // Keep original state in memory to test other values of `noun` and `verb`
-
-    let expected_result = 19690720;
-    let mut noun = 0;
-    let mut verb = 0;
-
-    'outer: loop {
-        // First, update steps 1 and 2 with aforementioned values
-        steps[1] = noun;
-        steps[2] = verb;
-
-
-        let mut program_finished_without_error = false;
-
-        // Next, execute the program until the value "99" is found
-        let mut current_step: usize = 0;
-
-        'inner: loop {
-            if steps.len() <= current_step {
-                eprintln!("Current step outside boundaries of input steps!");
-                break 'inner;
-            }
-            match steps[current_step] {
-                1 => {
-                    if steps.len() <= current_step + 3 {
-                        eprintln!("Current step + expected parameters outside boundaries of input steps!");
-                        break 'inner;
-                    }
-                    let first_step_position = steps[current_step + 1];
-                    let second_step_position = steps[current_step + 2];
-                    let destination_step_position = steps[current_step + 3];
-                    if steps.len() <= first_step_position {
-                        eprintln!("First parameter position outside boundaries of input steps!");
-                        break 'inner;
-                    }
-                    if steps.len() <= second_step_position {
-                        eprintln!("Second parameter position outside boundaries of input steps!");
-                        break 'inner;
-                    }
-                    steps[destination_step_position] = steps[first_step_position] + steps[second_step_position];
-                }
-                2 => {
-                    if steps.len() <= current_step + 3 {
-                        eprintln!("Current step + expected parameters outside boundaries of input steps!");
-                        break 'inner;
-                    }
-                    let first_step_position = steps[current_step + 1];
-                    let second_step_position = steps[current_step + 2];
-                    let destination_step_position = steps[current_step + 3];
-                    if steps.len() <= first_step_position {
-                        eprintln!("First parameter position outside boundaries of input steps!");
-                        break 'inner;
-                    }
-                    if steps.len() <= second_step_position {
-                        eprintln!("Second parameter position outside boundaries of input steps!");
-                        break 'inner;
-                    }
-                    steps[destination_step_position] = steps[first_step_position] * steps[second_step_position];
-                }
-                99 => {
-                    program_finished_without_error = true;
-                    break 'inner;
-                }
-                _ => {
-                    eprintln!("Unknown opcode : {:?}", steps[current_step]);
-                    break 'inner;
-                }
-            }
-
-            current_step += 4;
-        }
-
-        if program_finished_without_error && steps[0] == expected_result {
-            writeln!(io::stdout(), "Part 2 : {}", 100 * noun + verb)?;
-            break 'outer;
-        } else {
-            steps = original_steps.clone();
-            verb += 1;
-            if verb > 99 {
-                verb = 0;
-                noun += 1;
-                if noun > 99 {
-                    eprintln!("Noun > 99 !");
-                    break 'outer;
-                }
-            }
-        }
-
-    }
-
-    Ok(())
+    Err(Box::<Error>::from(format!("IntCode program ended unexpectedly!")))
 }
