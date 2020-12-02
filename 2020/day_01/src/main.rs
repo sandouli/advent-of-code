3716602240785ea821570e3ead27b0098ad7738b
sandouli <calvez.freddy@gmail.com>
2020-12-02 16:09:01 +0100

    Adapted code to benchmarks

--- 2020/day_01/src/main.rs
+++ 2020/day_01/src/main.rs
@@ -1,5 +1,9 @@
-use std::io::{self, Read, Write};
+#![cfg_attr(feature = "unstable", feature(test))]
+// Use this command to launch the program : cargo run --release < input/input.txt
+// Use this command to benchmark : cargo +nightly bench --features "unstable"
+
 use std::error::Error;
+use std::io::{self, Read, Write};
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
@@ -13,8 +17,8 @@ fn main() -> Result<()> {
 
     let expense_report = parse_input(&input)?;
 
-    part_1(&expense_report)?;
-    part_2(&expense_report)?;
+    writeln!(io::stdout(), "Part 1 : {}", part_1(&expense_report)?)?;
+    writeln!(io::stdout(), "Part 2 : {}", part_2(&expense_report)?)?;
     Ok(())
 }
 
@@ -26,12 +30,11 @@ fn parse_input(input: &str) -> Result<Vec<usize>> {
     Ok(expense_report)
 }
 
-fn part_1(expense_report: &[usize]) -> Result<()> {
+fn part_1(expense_report: &[usize]) -> Result<usize> {
     for i in 0..(expense_report.len() - 1) {
         for j in (i + 1)..expense_report.len() {
             if expense_report[i] + expense_report[j] == 2020 {
-                writeln!(io::stdout(), "Part 1 : {}", expense_report[i] * expense_report[j])?;
-                return Ok(());
+                return Ok(expense_report[i] * expense_report[j]);
             }
         }
     }
@@ -39,13 +42,12 @@ fn part_1(expense_report: &[usize]) -> Result<()> {
     err!("Part 1 : No combination found!")
 }
 
-fn part_2(expense_report: &[usize]) -> Result<()> {
+fn part_2(expense_report: &[usize]) -> Result<usize> {
     for i in 0..(expense_report.len() - 2) {
         for j in (i + 1)..(expense_report.len() - 1) {
             for k in (j + 1)..expense_report.len() {
                 if expense_report[i] + expense_report[j] + expense_report[k] == 2020 {
-                    writeln!(io::stdout(), "Part 2 : {}", expense_report[i] * expense_report[j] * expense_report[k])?;
-                    return Ok(());
+                    return Ok(expense_report[i] * expense_report[j] * expense_report[k]);
                 }
             }
         }
@@ -54,3 +56,31 @@ fn part_2(expense_report: &[usize]) -> Result<()> {
     err!("Part 2 : No combination found!")
 }
 
+#[cfg(all(feature = "unstable", test))]
+mod bench {
+    extern crate test;
+
+    use super::*;
+    use std::fs::File;
+    use test::Bencher;
+
+    fn open_input_file() -> Result<Vec<usize>> {
+        let mut input = String::new();
+        File::open("input/input.txt")?.read_to_string(&mut input)?;
+        parse_input(&input)
+    }
+
+    #[bench]
+    fn bench_part_1(b: &mut Bencher) -> Result<()> {
+        let expense_report = open_input_file()?;
+        b.iter(|| test::black_box(part_1(&expense_report)));
+        Ok(())
+    }
+
+    #[bench]
+    fn bench_part_2(b: &mut Bencher) -> Result<()> {
+        let expense_report = open_input_file()?;
+        b.iter(|| test::black_box(part_2(&expense_report)));
+        Ok(())
+    }
+}
